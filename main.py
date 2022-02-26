c46dada281abe7408b7808b328e985c84ee015ee
poochiekittie <100466443+poochiekittie@users.noreply.github.com>
2022-02-26 20:13:56 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
 import streamlit as st
 
+@st.cache
+def load_stuff():
+    os.system('!curl https://raw.githubusercontent.com/b3n-j4m1n/salary-seeker/master/salary-seeker.sh > salary-seeker.sh')
+    os.system('!chmod u+x *.sh')  
 
-st.markdown('hello world')
+load_stuff()
+st.markdown('hello world2')
